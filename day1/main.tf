aa26aec374ae4daef2e9b2e84180dff94bb98792
bujja sowjanya <bujjasowjanya672@gmail.com>
2024-08-27 12:18:21 +0530

    adding files

--- day1/main.tf
+++ day1/main.tf
@@ -1,3 +1,4 @@
 resource "aws_s3_bucket" "name" {
-  bucket = "test123terraform"
+
+  bucket = "rahulll"
 }
---
+++ day8-dependent-block/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "name" {
+    ami = "ami-02b49a24cfb95941c"
+    instance_type = "t2.micro"
+    key_name = "keypair"
+       depends_on = [ aws_s3_bucket.name ]
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "manasaaa"
+
+  
+}
